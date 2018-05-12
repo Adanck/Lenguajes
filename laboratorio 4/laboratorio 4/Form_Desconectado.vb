f1a3be887f8935253ee0013a90f7a76fd3107f5f
Adan Talavera <adan_talavera@hotmail.com>
2018-05-11 21:03:51 -0600

    nbg

---
+++ laboratorio 4/laboratorio 4/Form_Desconectado.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Imports System.Data.SqlClient
+Imports System.Data
+
+
+Public Class Form_Desconectado
+
+    Dim strNombre As String
+    Dim strApellido As String
+    Dim strApellido2 As String
+
+    Dim conn As New SqlConnection(My.Settings.conn)
+    Dim adapter As SqlDataAdapter
+    Dim table As New DataTable
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Public Function createInsert() As SqlClient.SqlCommand
+
+        Dim cmdInsert As New SqlClient.SqlCommand
+        cmdInsert.Connection = conn
+        cmdInsert.CommandText = "Insert into persona(nombre, Apellido1, Apellido2)" & "values (@nom,@ape1,@ape2)"
+        cmdInsert.Parameters.Add(New SqlClient.SqlParameter("@nom", SqlDbType.NVarChar, 50, "nombre"))
+        cmdInsert.Parameters.Add(New SqlClient.SqlParameter("@ape1", SqlDbType.NVarChar, 50, "Apellido1"))
+        cmdInsert.Parameters.Add(New SqlClient.SqlParameter("@ape2", SqlDbType.NVarChar, 50, "Apellido2"))
+        Return cmdInsert
+
+    End Function
+    Private Sub reLoad()
+        Dim fileReader As StreamReader
+        Dim line As String
+
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("50000 Registros.txt")
+        line = fileReader.ReadLine
+
+        Do While line <> Nothing
+            Dim vector = line.Split(" ")
+            If (vector.Length > 3) Then
+                strNombre = vector(0) + " " + vector(1)
+                strApellido = vector(2)
+                strApellido2 = vector(3)
+            Else
+                strNombre = vector(0)
+                strApellido = vector(1)
+                strApellido2 = vector(2)
+            End If
+        Loop
+
+
+
+    End Sub
+End Class
