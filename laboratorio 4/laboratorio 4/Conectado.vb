3be97083ee7f1b95e263b1e537b4ff6bf1a3d5c4
Yerick Ramirez <yerick.ramirez@ucrso.info>
2018-05-11 19:46:23 -0600

    Que sirva

--- laboratorio 4/laboratorio 4/Conectado.vb
+++ laboratorio 4/laboratorio 4/Conectado.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Data.SqlClient
 Imports System.IO
+Imports System.Data
 
 Public Class Conectado
 
@@ -9,23 +10,25 @@ Public Class Conectado
     Private strNombre As String
     Private strApellido1 As String
     Private strApellido2 As String
+    Private adpAdaptador As SqlDataAdapter
+    Private cadena As String = My.Settings.MyConnectio
 
     Private Sub cargarConectado()
 
-        Dim cadena As String = My.Settings.MyConnectio
         connection.ConnectionString = cadena
-
-
 
         Dim fileReader As StreamReader
         Dim line As String
-
         fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\yeric\Downloads\UCR\10 Registros.txt")
         line = fileReader.ReadLine
 
         tblDatosPersonas.Columns.Add("Nombre")
         tblDatosPersonas.Columns.Add("Apellido1")
         tblDatosPersonas.Columns.Add("Apellido2")
+
+        openConnection()
+        adpAdaptador.Fill(tblDatosPersonas)
+        closeConnection()
 
         Do While line <> Nothing
             Dim vector = line.Split(" ")
@@ -44,25 +47,35 @@ Public Class Conectado
             End If
             tblDatosPersonas.Rows.Add(strNombre, strApellido1, strApellido2)
             line = fileReader.ReadLine
+
+            adpAdaptador.InsertCommand = createInsert()
+            openConnection()
+            adpAdaptador.Update(tblDatosPersonas)
+            closeConnection()
         Loop
 
 
-        For Each row In tblDatosPersonas.Rows
-            Dim column As DataColumn
-            For Each column In tblDatosPersonas.Columns
-                Console.WriteLine(row(column))
-                Dim sentencias As New SqlCommand("Insert into Nombres(Nombre, Apellido1, Apellido2) values(@nom, @ape1, @ape2)", connection)
-
-            Next column
-        Next row
-
-
-
-        For i = 0 To tblDatosPersonas.Rows.Count
-            Dim sentencias As New SqlCommand("Insert into Nombres(Nombre, Apellido1, Apellido2) values(@nom, @ape1, @ape2)", connection)
-            sentencias.Parameters.AddWithValue("@nom", tblDatosPersonas.
-        Next
-
     End Sub
+
+    Private Function createInsert()
+        Dim cmdinsert As New SqlCommand("INSERT INTO Nombres (Nombre, Apellido1, Apellido2) VALUES (@nom, @ape1, @ape2)", connection)
+        cmdinsert.Parameters.Add(New SqlParameter("@nom", SqlDbType.NVarChar, 20, "Nombre"))
+        cmdinsert.Parameters.Add(New SqlParameter("@ape1", SqlDbType.NVarChar, 100, "Apellido1"))
+        cmdinsert.Parameters.Add(New SqlParameter("@ape2", SqlDbType.NVarChar, 200, "Apellido2"))
+        Return cmdinsert
+    End Function
+
+    Private Sub openConnection()
+        If connection.State <> ConnectionState.Open Then
+            connection.Open()
+        End If
+    End Sub
+
+    Private Sub closeConnection()
+        If connection.State <> ConnectionState.Closed Then
+            connection.Close()
+        End If
+    End Sub
+
 
 End Class
--- laboratorio 4/laboratorio 4/My Project/Settings.Designer.vb
+++ laboratorio 4/laboratorio 4/My Project/Settings.Designer.vb
@@ -13,67 +13,67 @@ Option Explicit On
 
 
 Namespace My
-
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0"),
-     Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>
+    
+    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0"),  _
+     Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
-
-        Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings()), MySettings)
-
+        
+        Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings()),MySettings)
+        
 #Region "My.Settings Auto-Save Functionality"
-#If _MYTYPE = "WindowsForms" Then
-        Private Shared addedHandler As Boolean
+#If _MyType = "WindowsForms" Then
+    Private Shared addedHandler As Boolean
 
-        Private Shared addedHandlerLockObject As New Object
+    Private Shared addedHandlerLockObject As New Object
 
-        <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-        Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
-            If My.Application.SaveMySettingsOnExit Then
-                My.Settings.Save()
-            End If
-        End Sub
+    <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
+    Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
+        If My.Application.SaveMySettingsOnExit Then
+            My.Settings.Save()
+        End If
+    End Sub
 #End If
 #End Region
-
+        
         Public Shared ReadOnly Property [Default]() As MySettings
             Get
-
-#If _MYTYPE = "WindowsForms" Then
-                   If Not addedHandler Then
-                        SyncLock addedHandlerLockObject
-                            If Not addedHandler Then
-                                AddHandler My.Application.Shutdown, AddressOf AutoSaveSettings
-                                addedHandler = True
-                            End If
-                        End SyncLock
-                    End If
+                
+#If _MyType = "WindowsForms" Then
+               If Not addedHandler Then
+                    SyncLock addedHandlerLockObject
+                        If Not addedHandler Then
+                            AddHandler My.Application.Shutdown, AddressOf AutoSaveSettings
+                            addedHandler = True
+                        End If
+                    End SyncLock
+                End If
 #End If
                 Return defaultInstance
             End Get
         End Property
-
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=ListaPersonas;Integrated Security=true")>
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=ListaPersonas;Integrated Security=true")>  _
         Public ReadOnly Property MyConnectio() As String
             Get
-                Return CType(Me("MyConnectio"), String)
+                Return CType(Me("MyConnectio"),String)
             End Get
         End Property
     End Class
 End Namespace
 
 Namespace My
-
-    <Global.Microsoft.VisualBasic.HideModuleNameAttribute(),
-     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),
-     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>
+    
+    <Global.Microsoft.VisualBasic.HideModuleNameAttribute(),  _
+     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
-
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>
+        
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
         Friend ReadOnly Property Settings() As Global.laboratorio_4.My.MySettings
             Get
                 Return Global.laboratorio_4.My.MySettings.Default
